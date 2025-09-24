4218162c63ee9322580810f68f2b37a696a5dc0d
Yuan Yuhao <325063151@qq.com>
2025-09-23 22:39:46 -0400

    change to use data2

---
+++ test2.r
@@ -0,0 +1,45 @@
+# library(keyATM) # use source code
+unloadNamespace("keyATM")
+rm(list = ls())
+devtools::load_all("./")
+
+library(quanteda)
+library(magrittr)
+data(data_corpus_inaugural, package = "quanteda")
+data_corpus_inaugural <- head(data_corpus_inaugural, n = 58)
+
+data_tokens <- tokens(
+    data_corpus_inaugural,
+    remove_numbers = TRUE,
+    remove_punct = TRUE,
+    remove_symbols = TRUE,
+    remove_separators = TRUE,
+    remove_url = TRUE
+) %>%
+    tokens_tolower() %>%
+    tokens_remove(
+        c(
+            stopwords("english"),
+            "may", "shall", "can",
+            "must", "upon", "with", "without"
+        )
+    ) %>%
+    tokens_select(min_nchar = 3)
+
+data_dfm <- dfm(data_tokens) %>%
+    dfm_trim(min_termfreq = 5, min_docfreq = 2)
+ncol(data_dfm) # the number of unique words
+
+keyATM_docs <- keyATM_read(texts = data_dfm)
+summary(keyATM_docs)
+
+dfm_mat <- convert(data_dfm, to = "data.frame")
+save(dfm_mat, file = "dfm_mat_2.rda")
+
+keywords <- list(
+    Government     = c("laws", "law", "executive"),
+    Congress       = c("congress", "party"),
+    Peace          = c("peace", "world", "freedom"),
+    Constitution   = c("constitution", "rights"),
+    ForeignAffairs = c("foreign", "war")
+)
